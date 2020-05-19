e824868d2323324bd5bc2da8762dd08f569e94f8
Dave <dave@metlife.co.jp>
2020-05-19 18:47:17 +0900

    Added degree and radian part

--- complex_number.py
+++ complex_number.py
@@ -1,3 +1,5 @@
+import math
+
 class ComplexNumber(object):
     def __init__(self, real, imaginary):
         self.real_component = int(real) if isinstance(real, int) else float(real)
@@ -38,6 +40,9 @@ class ComplexNumber(object):
     def __truediv__(self, other):
         if not self._is_complex_number(other):
             return
+        if self.real_component == 0 and self.imaginary_component == 0:
+            return None
+
         # self: (a1, b1) where a is real component, and b is imaginary component
         # other: (a2, b2) where a is real component, and b is imaginary component
         a1a2 = self.real_component * other.real_component
@@ -46,22 +51,31 @@ class ComplexNumber(object):
         a2b1 = self.imaginary_component * other.real_component
         a2_squared = other.real_component ** 2
         b2_squared = other.imaginary_component ** 2
-
+        
         return ComplexNumber((a1a2 + b1b2) / (a2_squared + b2_squared), (a2b1 - a1b2) / (a2_squared + b2_squared))
 
     def modulus(self):
-        import math
         return math.sqrt(self.real_component ** 2 + self.imaginary_component ** 2)
 
     def conjugate(self):
         return ComplexNumber(self.real_component, (-1) * self.imaginary_component)
-        # return self.__mul__(other)
 
     def _is_complex_number(self, other):
         if not isinstance(other, ComplexNumber):
             print("Invalid complex number.")
             return False
         return True
+    
+    def get_degree(self):
+        return math.degrees(self.get_radian())
+    
+    def get_radian(self):
+        if self.real_component == 0:
+            return None
+        return math.atan(self.imaginary_component/self.real_component)
+    
+    def get_length(self):
+        return self.modulus()
 
     def to_string(self):
         return str(self)
--- test_complex_number.py
+++ test_complex_number.py
@@ -4,9 +4,13 @@ from complex_number import ComplexNumber
 
 
 class TestComplexNumber(TestCase):
+    def setUp(self):
+        self.cn1 = ComplexNumber(3, 2)
+        self.cn2 = ComplexNumber(-1, -1)
+
     def test__mathematical_operations(self):
-        c1 = ComplexNumber(3, 2)
-        c2 = ComplexNumber(-1, -1)
+        c1 = self.cn1
+        c2 = self.cn2
 
         self.assertEqual("2+i", (c1+c2).to_string())
         self.assertEqual("4+3i", (c1-c2).to_string())
@@ -16,3 +20,26 @@ class TestComplexNumber(TestCase):
         self.assertEqual(1.4142135623730951, c2.modulus())
         self.assertEqual("3-2i", c1.conjugate().to_string())
         self.assertEqual("-1+i", c2.conjugate().to_string())
+    
+    def test__edge_case_for_mathematical_operations(self):
+        c1 = ComplexNumber(0, 0)
+        c2 = ComplexNumber(0, 0)
+        
+        self.assertEqual("0", (c1+c2).to_string())
+        self.assertEqual("0", (c1-c2).to_string())
+        self.assertEqual("0", (c1*c2).to_string())
+        self.assertEqual(None, (c1/c2))
+        self.assertEqual(0, c1.modulus())
+        self.assertEqual(0, c2.modulus())
+        self.assertEqual("0", c1.conjugate().to_string())
+        self.assertEqual("0", c2.conjugate().to_string())
+    
+    def test__get_length(self):
+        c1 = ComplexNumber(3, 4)
+        c2 = ComplexNumber(1, 1)
+        self.assertEquals(5.0, c1.get_length())
+        self.assertEquals(1.4142135623730951, c2.get_length())
+
+    def test__get_angle(self):
+        c1 = ComplexNumber(1, 1)
+        self.assertEquals(45.0, c1.get_degree())
